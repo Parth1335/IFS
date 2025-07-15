cb9724e4be7ab0ce0e21f73f4da1aa407feb6c0d
Parth1335 <131842721+Parth1335@users.noreply.github.com>
2025-07-15 23:52:43 +0530

    Update settings.py

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -147,5 +147,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://ifs-qi90.onrender.com",
+    "https://zippy-syrniki-7e533d.netlify.app/",
 ]
