4e9d4d888cb60d50bfa54d769a6b5f94e285dea5
Parth1335 <131842721+Parth1335@users.noreply.github.com>
2025-07-16 01:26:09 +0530

    Update axios.js

--- Frontend/feedback-ui/src/api/axios.js
+++ Frontend/feedback-ui/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const API_BASE = import.meta.env.VITE_API_BASE; 
 const api = axios.create({
-  baseURL: '${API_BASE}/api/',
+  baseURL: `${API_BASE}/api/`,
   headers: {
     'Content-Type': 'application/json',
   },
