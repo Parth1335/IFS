9ace22f3d4b2a593fe79e0427188e5bbd6690bbf
Parth1335 <131842721+Parth1335@users.noreply.github.com>
2025-07-16 02:51:28 +0530

    Update settings.py

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -148,7 +148,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://zippy-syrniki-7e533d.netlify.app/",
     "https://ifs232.netlify.app",
 ]
 CORS_ALLOW_HEADERS = list(default_headers) + [
