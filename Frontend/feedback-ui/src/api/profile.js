ee3b86d6efc538ad10b14325775afa7829044565
Parth1335 <131842721+Parth1335@users.noreply.github.com>
2025-07-16 01:46:16 +0530

    Update profile.js

--- Frontend/feedback-ui/src/api/profile.js
+++ Frontend/feedback-ui/src/api/profile.js
@@ -1,6 +1,7 @@
+const API_BASE = import.meta.env.VITE_API_BASE; 
 export const fetchUserProfile = async () => {
     const token = localStorage.getItem("accessToken");
-    const response = await fetch("http://localhost:8000/api/profile/", {
+    const response = await fetch(`${API_BASE}/api/profile/`, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
